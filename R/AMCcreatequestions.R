bbe777441302c9374dcef9f11283f4764e6fa415
nilsmy <nilsmyszkowskiscience@gmail.com>
2017-03-11 13:55:36 -0500

    ajout code auto avec code prefix

--- R/AMCcreatequestions.R
+++ R/AMCcreatequestions.R
@@ -1,10 +1,11 @@
 #' AMCcreatequestions
 #'
-#' @param element A character value or vector to define the category of the entire set of questions (character value) or of each question (character vector). Defaults to "general.
-#' @param code A character value or vector to identify each question (note that AMC requires each ode to be unique in a questionnaire).
 #' @param question A character value or vector containing the questions.
 #' @param correctanswers A character (value, vector) containing the correct answers. A vector (or list) of character vectors can also be passed, in the case of multiple correct answers.
 #' @param incorrectanswers A character (value, vector) containing the wrong answers. A vector (or list) of character vectors can also be passed, in the case of multiple wrong answers.
+#' @param element A character value or vector to define the category of the entire set of questions (character value) or of each question (character vector). Defaults to "general.
+#' @param code A character value or vector to identify each question (note that AMC requires each code to be unique in a questionnaire). Defaults to "Q1", "Q2", "Q3", etc. (the prefix "Q" can be changed with the "codeprefix" argument).
+#' @param codeprefix A character value to be used to generate automatically question codes, when not provided with the "code" argument.
 #' @param writefile A logical value indicating whether a .tex file should be created.
 #' @param filepath A character value containing the file path for the .tex file to be created (defaults to "questions.tex").
 #' @param multicols A numeric (or numeric vector) indicating the desired number of columns for the presentation of the correct and incorrect answers (note that the package multicols must be used in the final ".tex" document). Defaults to 1, which does not require the LaTeX multicols package.
@@ -17,7 +18,7 @@
 #'
 #' AMCcreatequestions("general",code,question,goodanswer1,list(wronganswer1, wronganswer2, wronganswer3, wronganswer4), multicols = multicols)
 #'
-AMCcreatequestions <- function(element = "general", code, question, correctanswers, incorrectanswers, writefile = F, filepath = "questions.tex", multicols=1) {
+AMCcreatequestions <- function(question, correctanswers, incorrectanswers, element = "general", code = paste(codeprefix,c(1:length(question)), sep=""), codeprefix = "Q", writefile = F, filepath = "questions.tex", multicols=1) {
 
   #ELEMENT
 
@@ -76,6 +77,9 @@ AMCcreatequestions <- function(element = "general", code, question, correctanswe
   codewronganswer <- function(x) {
     ifelse(x==""| is.na(x), "", paste("\\wrongchoice{", x, "}\n", sep = ""))
   }
+
+
+
 
   # Apply that function to the list of wrong answers
   arrayofcodedincorrectanswers <- sapply(FUN = codewronganswer, X = incorrectanswers, simplify = "array")
