22873cf46e20d4f8c9a827b552e3d2651fd1c6ee
nilsmy <nilsmyszkowskiscience@gmail.com>
2017-03-24 10:24:49 -0400

    Added vignette

--- vignettes/AMCTestmaker.R
+++ vignettes/AMCTestmaker.R
@@ -1,7 +1,40 @@
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
+## ------------------------------------------------------------------------
+library(AMCTestmakeR)
 
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+## ------------------------------------------------------------------------
+AMCcreatequestions(question = "How much is $1+1$?",
+                   correctanswers = 2,
+                   incorrectanswers = list(3, 11))
+
+## ------------------------------------------------------------------------
+question <- c("How much is $1+1$ ?",
+              "How much is $1 \\times 1$ ?",
+              "How much is $\\frac{1}{2}$ ?")
+correct <- c(2,1,0.5)
+incorrect1 <- c(3,4,10)
+incorrect2 <- c(1,3,100)
+incorrect3 <- c(4,8,NA)
+
+## ------------------------------------------------------------------------
+AMCcreatequestions(question = question,
+   correctanswers = correct,
+   incorrectanswers = list(incorrect1,incorrect2,incorrect3))
+
+## ------------------------------------------------------------------------
+AMCcreatequestions(element = c("ADD", "MULT", "DIV"),
+   question = question,
+   correctanswers = correct,
+   incorrectanswers = list(incorrect1,incorrect2,incorrect3))
+
+## ------------------------------------------------------------------------
+AMCcreatequestions(code = c("ADD1", "MULT1", "DIV1"),
+   question = question,
+   correctanswers = correct,
+   incorrectanswers = list(incorrect1,incorrect2,incorrect3))
+
+## ------------------------------------------------------------------------
+AMCcreatequestions(codeprefix = "MATH",
+   question = question,
+   correctanswers = correct,
+   incorrectanswers = list(incorrect1,incorrect2,incorrect3))
 
